7d53129a6f086ebcd0b19e89d34b79c6a38e9591
Archana Berry OPS!!! <157275787+archanaberry@users.noreply.github.com>
2024-06-03 17:57:46 +0700

    Update archanaberry_express.hpp

--- libarchanaberry/archanaberry_express.hpp
+++ libarchanaberry/archanaberry_express.hpp
@@ -1,36 +1,38 @@
 #ifndef ARCHANABERRY_EXPRESS_HPP
 #define ARCHANABERRY_EXPRESS_HPP
 
+#include <iostream>
 #include <string>
 #include <functional>
 #include <unordered_map>
-#include <thread>
-#include <vector>
-#include <mutex>
 #include <boost/asio.hpp>
+#include "archanaberry_mimetypes.hpp" // Tambahkan include
 
 namespace archanaberry {
-namespace express {
+    namespace express {
 
-class AppWeb {
-public:
-    using RouteHandler = std::function<void(boost::asio::ip::tcp::iostream&)>;
+        class AppWeb {
+        public:
+            using Handler = std::function<void(std::ostream&)>;
 
-    AppWeb();
+            AppWeb() {}
 
-    void get(const std::string& path, RouteHandler handler);
+            void get(const std::string& path, Handler handler);
+            void serveStatic(const std::string& directory);
 
-    void listen(int port);
+            void listen(int port);
 
-private:
-    void handleRequest(boost::asio::ip::tcp::socket socket);
+        private:
+            void handle_request(const std::string& request, std::ostream& response_stream);
+            void serve_file(const std::string& path, std::ostream& response_stream);
 
-    std::unordered_map<std::string, RouteHandler> routes_;
-    std::vector<std::thread> thread_pool_;
-    std::mutex mutex_;
-};
+            std::unordered_map<std::string, Handler> routes_;
+            std::string static_directory_;
+        };
 
-} // namespace express
+        extern AppWeb appweb;
+
+    } // namespace express
 } // namespace archanaberry
 
 #endif // ARCHANABERRY_EXPRESS_HPP
